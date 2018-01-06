8b713be075e4b6c37cf8d870081ed72796a5666e
simboyy <smkorera@gmail.com>
2018-01-06 21:26:52 +0200

    Update product.controller.js

--- server/api/product/product.controller.js
+++ server/api/product/product.controller.js
@@ -184,7 +184,7 @@ function index(req, res) {
     var select = isJson(req.query.select);
 
     var p = [];
-    _product2.default.find(q).limit(req.query.limit).skip(req.query.skip).sort(sort).sort({name: -1}).select(select).exec(function (err, products) {
+    _product2.default.find(q).limit(req.query.limit).skip(req.query.skip).sort(sort).sort({logo: -1}).select(select).exec(function (err, products) {
       if (err) {
         return handleError(res, err);
       }
